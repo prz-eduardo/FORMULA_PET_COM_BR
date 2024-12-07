2720a95502b8c932ea1c9ed19c87f77066f8e2bb
Eduardo dos Prazeres <113868646+prz-eduardo@users.noreply.github.com>
2024-12-07 12:18:40 -0300

    feat : hero slider

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { register } from 'swiper/element/bundle';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
   imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HeroComponent {
+
+  constructor() {
+    register();
+  }
 
 }
