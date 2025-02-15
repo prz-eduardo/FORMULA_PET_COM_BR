d0cb23052b532d10eb0b1a495a29d34de5ccb903
Eduardo dos Prazeres <113868646+prz-eduardo@users.noreply.github.com>
2025-02-15 16:04:51 -0300

    style cards

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,27 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 @Component({
   selector: 'app-product-card',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.scss'
+  styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent implements OnInit {
-@Input() id: number | undefined;
-@Input() category: string | undefined;
-@Input() customizations: { dosage: string[], packaging: string[] } | undefined;
-@Input() description: string | undefined;
-@Input() image: string | undefined;
-@Input() name: string | undefined;
-@Input() price: number | undefined;
+  @Input() id: number | undefined;
+  @Input() category: string | undefined;
+  @Input() customizations: any | undefined; // Pode conter: { dosage, packaging } ou { size, scent }
+  @Input() description: string | undefined;
+  @Input() image: string | undefined;
+  @Input() name: string | undefined;
+  @Input() price: number | undefined;
+  @Input() rating: number | undefined;
+  @Input() stock: number | undefined;
+  @Input() tags: string[] | undefined;
+  @Input() weight: string | undefined;
+  @Input() isFavourite: boolean | undefined;
+  @Input() isAddedToCart: boolean | undefined;
+  @Input() discount: number | undefined;
 
+  constructor() {}
 
-constructor() {
- 
-}
-
-ngOnInit(): void {
- }
+  ngOnInit(): void {}
 }
