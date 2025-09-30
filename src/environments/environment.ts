6974100e98e1fad62972102fbfa899a627e5b133
Eduardo dos Prazeres <113868646+prz-eduardo@users.noreply.github.com>
2025-09-30 00:41:17 -0300

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiBaseUrl : 'http://localhost:8080'
-  apiBaseUrl: 'https://formulapetcombrbackend-production.up.railway.app/api' // substitua pelo link do Railway
+  apiBaseUrl: 'https://formulapetcombrbackend-production.up.railway.app' // substitua pelo link do Railway
 };
